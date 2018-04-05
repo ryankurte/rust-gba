c426b8ceca93ca8b0fc291109397a61de13cb443
Ryan Kurte <ryankurte@gmail.com>
2018-04-05 16:40:35 +1200

    Type based model for graphics modes

--- src/graphics/colour.rs
+++ src/graphics/colour.rs
@@ -1,24 +1,41 @@
-
+// rust-gba
+// 16-bit colour implementation
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Colour (u16);
 
+// Helper macro to build static colour definitions
+macro_rules! rgb16 {
+    ($r:expr, $g:expr, $b:expr) => ((($r & 0x1F) as u16) | 
+                                    ((($g & 0x1F) as u16) << 5) |
+                                    ((($b & 0x1F) as u16) << 10))
+}
+
+// 16-bit colour (5 bits per channel)
 impl Colour {
+    pub const MAX: u8 = 0x1F;
+
+    // Create a new colour from 16-bit representation
     pub fn new(v: u16) -> Colour {
         Colour(v)
     }
 
-    // Creates a colour for mode3 with 16 bits, 5 bits for each channel.
+    // Create a new colour from individual channels
     pub fn rgb(red: u8, green: u8, blue: u8) -> Colour {
-        Colour(
-            ((red & 0x1F) as u16) | 
-            (((green & 0x1F) as u16) << 5) |
-            (((blue & 0x1F) as u16) << 10)
-        )
+        Colour(rgb16!(red, green, blue))
     }
 
+    // Fetch u16 representation
     pub fn u16(&self) -> u16 {
         self.0
     }
+
+    // Helper colours
+    pub const R: Colour = Colour(rgb16!(Self::MAX, 0, 0));
+    pub const O: Colour = Colour(rgb16!(Self::MAX, Self::MAX/2, 0));
+    pub const Y: Colour = Colour(rgb16!(Self::MAX, Self::MAX, 0));
+    pub const G: Colour = Colour(rgb16!(0, Self::MAX, 0));
+    pub const B: Colour = Colour(rgb16!(0, 0, Self::MAX));
+    pub const V: Colour = Colour(rgb16!(Self::MAX/2, 0, Self::MAX));
 }
 
--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -6,12 +6,41 @@ pub mod colour;
 pub mod bitmap;
 pub mod tilemap;
 
+use graphics::bitmap::{BitmapMode, Mode3};
+
 #[derive(Debug, PartialEq)]
-pub enum Graphics {
+pub enum Mode {
+    NONE,
     MODE1,
     MODE2,
-    MODE3(bitmap::Mode3)
+    MODE3
 }
 
+pub enum None {}
 
+pub struct Graphics<T> {
+    mode: T
+}
 
+// Methods for all graphics implementations
+impl <T>Graphics<T> {
+    // Fetch active graphics mode
+    pub fn active(&mut self) -> &mut T {
+        &mut self.mode
+    }
+    
+    // Set graphics to mode 3
+    pub fn mode3(mut self) -> Graphics<bitmap::Mode3> {
+        Graphics::new()
+    }
+}
+
+// Methods for bitmap::Mode3 graphics
+impl Graphics<bitmap::Mode3> {
+    // Create a new Mode3 graphics instance
+    pub fn new() -> Graphics<bitmap::Mode3> {
+        let mut mode = bitmap::Mode3::new();
+        mode.enable();
+        Graphics::<bitmap::Mode3>{mode: mode}
+    }
+}
